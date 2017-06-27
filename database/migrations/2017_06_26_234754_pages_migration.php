4f57190750ee86eff4198d77355ca7820f0b1f8b
Michael Bukachi <michaelbukachi@gmail.com>
2017-06-27 22:10:14 +0300

    fixed bugs in migrations

--- database/migrations/2017_06_26_234754_pages_migration.php
+++ database/migrations/2017_06_26_234754_pages_migration.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class PagesMigration extends Migration
 {
@@ -13,9 +13,10 @@ class PagesMigration extends Migration
      */
     public function up()
     {
-        Schema::create('Pages',function (Blueprint $table){
-           $table->String('page');
-           $table->timestamp('create_at');
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('page');
+            $table->timestamps();
 
         });
     }
