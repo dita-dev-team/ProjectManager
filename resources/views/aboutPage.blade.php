3628a852d093223adcef2312c46d7031ac9b12ce
mtotodev05 <mtotodev05@gmail.com>
2017-06-27 03:02:35 +0300

    Migration::Pages

--- resources/views/aboutPage.blade.php
+++ resources/views/aboutPage.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>DITA::About Project Manager</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -69,7 +69,6 @@
             About Project Manager
         </h3>
     </div>
-
 
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,9 @@
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
+                    <a href="aboutPage.blade.php">About</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+
                 </div>
             </div>
         </div>
