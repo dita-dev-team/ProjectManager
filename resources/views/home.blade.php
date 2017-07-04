fa4dc98a4045d6f56539d2251be8767c1341b4d2
md-arin <mainadanarin@gmail.com>
2017-07-04 10:26:52 +0300

    Edit Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.app')
-
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
 
-                    <div class="panel-body">
-                        You are logged in!
-                    </div>
-                </div>
-            </div>
+    <div class="heading">
+        <h1>Project Manager</h1>
+    </div>
+
+    <div class="card card-block welcome-card">
+        <div class="carousel">
+            <img src="{{ asset('img/placeholder.png') }}" alt="" class="responsive">
+        </div>
+        <div class="well">
+            <ul>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+            </ul>
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ContentController@homeContent');
 Route::get('about',function (){
    return view('aboutPage');
 });
