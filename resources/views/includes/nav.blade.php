5e732281456f6c8a2b29e66af43f4a77826c62b8
mtotodev05 <mtotodev05@gmail.com>
2017-07-05 09:46:08 +0300

    Nav:Icons

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -9,14 +9,14 @@
         </ul>
         <ul class="nav nav-tabs pull-xs-right">
             <li class="nav-item">
-                <a class="nav-link" href="{{url('/')}}"><i class="icon-home" style="size:50px"></i></a>
+                <a class="nav-link" href="{{url('/')}}"><i class="icon-home"style="size:50px;margin-right: 10px"></i>Home</a>
             </li>
             <li class="nav-item">
 
-                <a class="nav-link" href="{{url('/about')}}"><i class="icon-question-sign"></i></a>
+                <a class="nav-link" href="{{url('/about')}}"><i class="icon-question-sign"style="margin-right:10px"></i>About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{'/tutorials'}}"><i class="icon-briefcase"></i></a>
+                <a class="nav-link" href="{{'/tutorials'}}"><i class="icon-facetime-video"style="margin-right: 10px"></i>Tutorials</a>
             </li>
         </ul>
     </div>
