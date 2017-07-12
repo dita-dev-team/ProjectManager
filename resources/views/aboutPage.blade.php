184d98eefde3e603501831f2843b963d210fa366
edwynmuchiri <eddmuchiri@gmailcom>
2017-07-12 18:02:08 +0300

    About team add

--- resources/views/aboutPage.blade.php
+++ resources/views/aboutPage.blade.php
@@ -21,4 +21,72 @@
             </div>
         </div>
     </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="text-center">
+                    PROJECT TEAM
+                </h3>
+            </div>
+        </div>
+        <div class="row" align="center">
+            <div class="col-md-4">
+                <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" class="img-circle"/>
+                <dl>
+                    <dt>
+                        Description lists
+                    </dt>
+                    <dd>
+                        A description list is perfect for defining terms.
+                    </dd>
+                    <dt>
+                        Euismod
+                    </dt>
+                    <dd>
+                        Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
+                    </dd>
+                    <dd>
+                        Donec id elit non mi porta gravida at eget metus.
+                    </dd>
+
+                </dl>
+            </div>
+            <div class="col-md-4" align="center">
+                <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" class="img-circle"/>
+                <dl>
+                    <dt>
+                        Description lists
+                    </dt>
+                    <dd>
+                        A description list is perfect for defining terms.
+                    </dd>
+                    <dt>
+                        Euismod
+                    </dt>
+                    <dd>
+                        Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
+                    </dd>
+
+                </dl>
+            </div>
+            <div class="col-md-4" align="center">
+                <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" class="img-circle"/>
+                <dl>
+                    <dt>
+                        Description lists
+                    </dt>
+                    <dd>
+                        A description list is perfect for defining terms.
+                    </dd>
+                    <dt>
+                        Euismod
+                    </dt>
+                    <dd>
+                        Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
+                    </dd>
+
+                </dl>
+            </div>
+    </div>
 @endsection
