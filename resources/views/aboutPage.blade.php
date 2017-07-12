eba1329c41127f63c349fb5b5fb1c5597ae4aada
md-arin <mainadanarin@gmail.com>
2017-07-12 18:15:16 +0300

    Update Project Team as part of container css class

--- resources/views/aboutPage.blade.php
+++ resources/views/aboutPage.blade.php
@@ -22,7 +22,7 @@
         </div>
     </div>
 
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <h3 class="text-center">
