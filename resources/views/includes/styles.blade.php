7fa2ee6c4ac7ad977d649b21c984866b0bbc2dea
md-arin <mainadanarin@gmail.com>
2017-07-12 18:41:38 +0300

    Minor Changes to feature_tutorialsPage

--- resources/views/includes/styles.blade.php
+++ resources/views/includes/styles.blade.php
@@ -1,12 +1,12 @@
 {{--fonts--}}
 {{--styles--}}
 <!-- Bootstrap Material Design -->
+<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
       integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
       integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.2/css/mdb.min.css">
-<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 
 <!-- Fonts -->
 {{--<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">--}}
