f3a0a704a55a26300e37935a2878dcd10a4e77d5
edwynmuchiri <eddmuchiri@gmailcom>
2017-07-12 18:26:06 +0300

    Button fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,7 +61,7 @@
                 Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
             </p>
             <p>
-                <a class="btn btn-large" back-ground:color="green" href="#">Learn more</a>
+                <a class="btn btn-large" href="#">Learn more</a>
             </p>
         </div>
         <div class="col-md-4" align="center">
@@ -72,7 +72,7 @@
                 Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
             </p>
             <p>
-                <a class="btn btn-primary btn-large" href="#">Learn more</a>
+                <a class="btn btn-large" href="#">Learn more</a>
             </p>
         </div>
         <div class="col-md-4" align="center">
@@ -83,7 +83,7 @@
                 Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
             </p>
             <p>
-                <a class="btn btn-primary btn-large" href="#">Learn more</a>
+                <a class="btn btn-large" href="#">Learn more</a>
             </p>
         </div>
     </div>
